e159b7a567c2c6cc58b6998316fbc92a402db6d5
Prakash Tamang <meprakashtamang@gmail.com>
2025-01-10 15:59:35 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
+
+const App = () => {
+  return (
+    <>
+    <Navbar></Navbar>
+
+    <Hero></Hero>
+
+    <HomeCards></HomeCards>
+
+    <JobListings></JobListings>
+
+    <ViewAllJobs></ViewAllJobs>
+    </>
+  )
+}
+
+export default App
