b51b99e6b2547aeba59c415807454fbcc1aa46f6
Prakash Tamang <meprakashtamang@gmail.com>
2025-01-11 16:42:04 +0545

    add homepage with component

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { FaMapMarker } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const JobListing = ({job}) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
@@ -21,7 +22,7 @@ const JobListing = ({job}) => {
         <div className="mb-5">
         {description}
         </div>
-        <button onClick={()=> setShowFullDescription((prevState)=>!prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">{ showFullDescription ? 'Less' : 'More' }</button>
+        <button onClick={()=> setShowFullDescription((prevState) => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">{ showFullDescription ? 'Less' : 'More' }</button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -32,12 +33,12 @@ const JobListing = ({job}) => {
             <FaMapMarker className="inline text-lg mb-1 mr-1"></FaMapMarker>
             {job.location}
             </div>
-            <a
-            href={`/job/${job.id}`}
+            <Link
+            to={`/job/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
             Read More
-            </a>
+            </Link>
         </div>
         </div>
     </div>
